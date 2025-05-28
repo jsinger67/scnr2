0627cd2d60784d65373c6a5e77e6cecd9ecc780b
Jörg Singer <singer.joerg@gmx.de>
2025-05-28 12:52:21 +0200

    Implement NFA structure and functionality in nfa.rs

--- scnr2_generate/src/lib.rs
+++ scnr2_generate/src/lib.rs
@@ -1,6 +1,10 @@
+pub type Error = Box<dyn std::error::Error>;
+
 /// The result type for the `scrn` crate.
-pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+pub type Result<T> = std::result::Result<T, crate::Error>;
+
+/// The nfa module contains the NFA implementation.
+mod nfa;
 
 /// The parser module contains the regex syntax parser.
 mod parser;
-pub(crate) use parser::parse_regex;
